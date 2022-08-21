a5c611d59a9ebdbe7e11af9a2ef22f5bad356eb0
Marek Küthe <m.k@mk16.de>
2022-08-21 17:41:38 +0200

    improve code quality
    Signed-off-by: Marek Küthe <m.k@mk16.de>

--- helper/generate_key.rb
+++ helper/generate_key.rb
@@ -1,49 +1,51 @@
 
-require "openssl"
-require "optparse"
+require 'openssl'
+require 'optparse'
 
 options = {}
 OptionParser.new do |opt|
-  opt.on("-t", "--type [RSA|EC]", "Sets the key type") { |o| options[:type] = o.downcase.to_sym }
-  opt.on("-s", "--size [SIZE]", "For RSA keys, the key length. Common are 2048, 4096 or 8192.") { |o| options[:rsa_size] = o }
-  opt.on("-c", "--curve [CURVE]", "For EC key, the curve.") { |o| options[:ec_curve] = o }
-  opt.on("-o", "--output [FILE]", "File in which the key is stored. (PEM format)") { |o| options[:output] = o }
-  opt.on("-lc", "Lists the available ec curves") { |o| options[:lc] = true }
+  opt.on('-t', '--type [RSA|EC]', 'Sets the key type') { |o| options[:type] = o.downcase.to_sym }
+  opt.on('-s', '--size [SIZE]', 'For RSA keys, the key length. Common are 2048, 4096 or 8192.') { |o| options[:rsa_size] = o }
+  opt.on('-c', '--curve [CURVE]', 'For EC key, the curve.') { |o| options[:ec_curve] = o }
+  opt.on('-o', '--output [FILE]', 'File in which the key is stored. (PEM format)') { |o| options[:output] = o }
+  opt.on('-lc', 'Lists the available ec curves') { options[:lc] = true }
 end.parse!
 
 if options[:lc]
-  puts "NAME\tDESCRIPTION"
-  OpenSSL::PKey::EC.builtin_curves.each { |curve|
+  puts 'NAME\tDESCRIPTION'
+  OpenSSL::PKey::EC.builtin_curves.each do |curve|
     puts "#{curve[0]}\t#{curve[1]}"
-  }
+  end
 elsif options[:type]
   output_file = options[:output]
-  
-  if ! output_file
-    raise RuntimeError, "No file was specified to save."
+
+  unless output_file
+    raise 'No file was specified to save.'
   end
-  
+
   case options[:type]
   when :rsa
-    puts "Generating rsa key..."
+    puts 'Generating rsa key...'
     size = options[:rsa_size].to_i
     if size < 1024
-      raise RuntimeError, "The size must be greater than 1024."
+      raise 'The size must be greater than 1024.'
     end
+
     key = OpenSSL::PKey::RSA.generate size
   when :ec
-    puts "Generating ec key..."
+    puts 'Generating ec key...'
     curve = options[:ec_curve]
-    if ! curve
-      raise RuntimeError, "No EC curve was specified."
+    unless curve
+      raise 'No EC curve was specified.'
     end
+
     key = OpenSSL::PKey::EC.generate curve
   else
-    raise RuntimeError, "Invalid type. Can be RSA or EC."
+    raise 'Invalid type. Can be RSA or EC.'
   end
-  
+
   File.write output_file, key.to_pem
   puts "Saved to #{output_file}"
 else
-  raise RuntimeError, "No arguments given. Try `-h` or `-t rsa -s 4096 -o key.pem`."
+  raise 'No arguments given. Try `-h` or `-t rsa -s 4096 -o key.pem`.'
 end
